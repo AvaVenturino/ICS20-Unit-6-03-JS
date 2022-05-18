5629bf3940e69d43ead8410794dfb6ed0045d3bd
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-05-18 16:07:29 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// Copyright (c) 2022 Ava Venturino All rights reserved
+//
+// Created by: Ava Venturino
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS20-Unit-6-02-JS/sw.js", {
+    scope: "/ICS20-Unit-6-02-JS/",
+  });
+}
+
+/**
+ * Input
+ */
+function myButtonClicked() {
+  if (localStorage.updateCookieCount) {
+    localStorage.updateCookieCount = Number(localStorage.updateCookieCount) + 1;
+  } else {
+    localStorage.updateCookieCount = +1;
+  }
+  document.getElementById("answer").innerHTML =
+    "You have clicked the cookie " + localStorage.updateCookieCount + " times";
+}
